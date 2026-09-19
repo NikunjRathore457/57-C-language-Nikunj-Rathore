b26651eb1563118ac2f0a53903135cacc9e6cc32
Nikunj Rathore <nikunjrathore9600@gmail.com>
2026-09-19 03:02:34 -0700

    Add Signed and Unsigned integer demonstration

---
+++ lab04/Signed_Unsigned.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    signed int a = -7;
+    unsigned int b = 12;
+
+    printf("Signed value = %d\n", a);
+    printf("Unsigned value = %u\n", b);
+
+    return 0;
+}
